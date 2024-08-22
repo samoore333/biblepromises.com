85bb1f299a79c45fc1f9124d133330482b7652e9
samoore333 <samoore333@gmail.com>
2024-08-22 16:32:52 -0700

    About Us Setup

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import HomePageView
+from .views import AboutUsView, HomePageView
 
 app_name = 'pages'
 urlpatterns = [
     path('', HomePageView.as_view(), name='homepage'),
+    path('about-us/', AboutUsView.as_view(), name='about-us'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -2,3 +2,6 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = 'pages/home.html'
+
+class AboutUsView(TemplateView):
+    template_name = 'pages/about_us.html'
